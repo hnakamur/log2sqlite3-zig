0b89852be83099b1ea334e7a81a376364bab6a1a
Hiroaki Nakamura <hnakamur@gmail.com>
2022-07-08 21:21:33 +0900

    Use sqlite3

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,26 @@
 const std = @import("std");
+const sqlite = @import("sqlite");
 
 pub fn main() anyerror!void {
     std.log.info("All your codebase are belong to us.", .{});
+    var db = try sqlite.Db.init(.{
+        .mode = sqlite.Db.Mode{ .File = "mydata.db" },
+        .open_flags = .{
+            .write = true,
+            .create = true,
+        },
+        .threading_mode = .MultiThread,
+    });
+    defer db.deinit();
+
+    const query =
+        \\CREATE TABLE IF NOT EXISTS table1 (col1 TEXT)
+    ;
+
+    var stmt = try db.prepare(query);
+    defer stmt.deinit();
+
+    try stmt.exec(.{}, .{});
 }
 
 test "basic test" {
